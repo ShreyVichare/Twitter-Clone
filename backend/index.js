1442cbba524d49044e8226c4667a0f7e20195172
Shrey Vichare <smashshrey@gmail.com>
2024-12-20 01:04:49 +0530

    MongoDB Cluster Created & Connection Initiated

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import databaseConnection from "./config/database.js";
 dotenv.config({
   path: ".env",
 });
+
+databaseConnection();
 const app = express();
 app.listen(process.env.PORT, () => {
   console.log(`Server listen at ${process.env.PORT}`);
