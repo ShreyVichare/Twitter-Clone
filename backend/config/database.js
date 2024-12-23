65d327ec3be9c94df963c36125f5f5ad6a93e293
Shrey Vichare <smashshrey@gmail.com>
2024-12-23 10:15:45 +0530

    login & register

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+dotenv.config({
+  path: "../.env",
+});
 const databaseConnection = () => {
-  dotenv.config({
-    path: "../.env",
-  });
   mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
 import databaseConnection from "./config/database.js";
+import cookieParser from "cookie-parser";
+import userRoute from "./routes/userRoute.js";
 dotenv.config({
   path: ".env",
 });
 
 databaseConnection();
 const app = express();
+
+//middlewares
+app.use(
+  express.urlencoded({
+    extends: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+//api
+app.use("/api/v1/user", userRoute);
+
+//api address http://localhost:8080/api/v1/
+
 app.listen(process.env.PORT, () => {
   console.log(`Server listen at ${process.env.PORT}`);
 });
