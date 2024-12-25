6bfc2bb2b04b7e123079bab397ffc63452298138
Shrey Vichare <smashshrey@gmail.com>
2024-12-26 00:41:07 +0530

    View Turf Page created

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import databaseConnection from "./config/database.js";
 import cookieParser from "cookie-parser";
 import userRoute from "./routes/userRoute.js";
 import tweetRoute from "./routes/tweetRoute.js";
+import cors from "cors";
 dotenv.config({
   path: ".env",
 });
@@ -14,13 +15,18 @@ const app = express();
 //middlewares
 app.use(
   express.urlencoded({
-    extends: true,
+    extended: true,
   })
 );
 
 app.use(express.json());
 app.use(cookieParser());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
 
+app.use(cors(corsOptions));
 //api
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/tweet", tweetRoute);
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,66 @@
 import React, { useState } from "react";
-
+import axios from "axios";
+import { USER_API_END_POINT } from "../utils/constant.js";
+import toast from "react-hot-toast";
 const Login = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const sumbitHandler = async (e) => {
+    e.preventDefault();
+    if (isLogin) {
+      //login
+      try {
+        const res = await axios.post(
+          `${USER_API_END_POINT}/login`,
+          {
+            email,
+            password,
+          },
+          {
+            headers: { "Content-Type": "application/json" },
+            withCredentials: true,
+          }
+        );
+        if (res.data.success) {
+          toast.success(res.data.message);
+        }
+        console.log(res);
+      } catch (error) {
+        toast.success(error.response.data.message);
+      }
+    } else {
+      //signup
+      try {
+        const res = await axios.post(
+          `${USER_API_END_POINT}/register`,
+          {
+            name,
+            username,
+            email,
+            password,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+            withCredentials: true,
+          }
+        );
+        if (res.data.success) {
+          toast.success(res.data.message);
+        }
+        console.log(res);
+      } catch (error) {
+        toast.success(error.response.data.message);
+
+        console.log(error);
+      }
+    }
+  };
 
   const loginSignupHandler = () => {
     setIsLogin(!isLogin);
@@ -24,15 +83,19 @@ const Login = () => {
           <h1 className="mt-4 mb-2 text-2xl font-bold ">
             {isLogin ? "Login" : "Sign up"}
           </h1>
-          <form className="flex flex-col w-[55%]">
+          <form onSubmit={sumbitHandler} className="flex flex-col w-[55%]">
             {!isLogin && (
               <>
                 <input
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                   type="text"
                   placeholder="Name"
                   className="outline-blue-500 my-1 border border-gray-800 px-3 py-2 rounded-full font-semibold"
                 />
                 <input
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
                   type="text"
                   placeholder="Username"
                   className="outline-blue-500 my-1 border border-gray-800 px-3 py-2 font-semibold rounded-full"
@@ -40,12 +103,16 @@ const Login = () => {
               </>
             )}
             <input
-              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
               placeholder="Email"
               className="outline-blue-500 my-1 border border-gray-800 font-semibold px-3 py-2 rounded-full"
             />
             <input
-              type="text"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              type="password"
               placeholder="Password"
               className="outline-blue-500 my-1 border font-semibold border-gray-800 px-3 py-2 rounded-full"
             />
