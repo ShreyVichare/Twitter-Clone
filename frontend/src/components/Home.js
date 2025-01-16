4c31612145c11a87cc002cc781ffdd9c07920bbe
Shrey Vichare <smashshrey@gmail.com>
2025-01-17 00:20:58 +0530

    Redux Persist added

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LeftSideBar from "./LeftSideBar";
 import RightSideBar from "./RightSideBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import useOtherUsers from "../hooks/useOtherUsers";
 import { useSelector } from "react-redux";
 import useGetMyTweets from "../hooks/useGetMyTweets";
 
 const Home = () => {
   const { user, otherUsers } = useSelector((store) => store.user);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, []);
   //Custom Hooks
   useOtherUsers(user?._id);
   useGetMyTweets(user?._id);
